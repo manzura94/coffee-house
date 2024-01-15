847e3759ecebcf26e8002ea2c5901a23be3ccd80
manzura94 <manzura_jabborova@mail.ru>
2024-01-15 19:31:47 +0500

    feat: hero added video

--- index.js
+++ index.js
@@ -1,81 +1,72 @@
+const burgerButton = document.querySelector('.header__burger-button');
+const lineButton = document.querySelector('.line');
+const burgerMenu = document.querySelector('.header__menu');
+const body = document.querySelector('.wrapper');
+const links = document.getElementsByClassName('header__menu-item');
 
-const burgerButton = document.querySelector(".header__burger-button");
-const lineButton = document.querySelector(".line");
-const burgerMenu = document.querySelector(".header__menu");
-const body = document.querySelector(".wrapper");
-const links = document.getElementsByClassName("header__menu-item")
+burgerButton.addEventListener('click', function () {
+    if (lineButton.classList.contains('active-menu')) {
+        lineButton.classList.remove('active-menu');
+    } else {
+        lineButton.classList.add('active-menu');
+    }
 
+    if (burgerMenu.classList.contains('showMenu')) {
+        burgerMenu.classList.remove('showMenu');
+    } else {
+        burgerMenu.classList.add('showMenu');
+    }
 
-burgerButton.addEventListener("click", function () {
-  if (lineButton.classList.contains("active-menu")) {
-    lineButton.classList.remove("active-menu");
-  } else {
-    lineButton.classList.add("active-menu");
-  }
-
-  if (burgerMenu.classList.contains("showMenu")) {
-    burgerMenu.classList.remove("showMenu");
-
-  } else {
-    burgerMenu.classList.add("showMenu");
-  }
-
-  if (body.classList.contains("no-scroll")) {
-    body.classList.remove("no-scroll");
-
-  } else {
-    body.classList.add("no-scroll");
-  }
+    if (body.classList.contains('no-scroll')) {
+        body.classList.remove('no-scroll');
+    } else {
+        body.classList.add('no-scroll');
+    }
 });
-
 
 for (let index = 0; index < links.length; index++) {
     const element = links[index];
 
-    element.addEventListener("click", function(){
-        lineButton.classList.remove("active-menu");
-        burgerMenu.classList.remove("showMenu");
-        body.classList.remove("no-scroll");
-    })
-    
+    element.addEventListener('click', function () {
+        lineButton.classList.remove('active-menu');
+        burgerMenu.classList.remove('showMenu');
+        body.classList.remove('no-scroll');
+    });
 }
 
 let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("favourite__slides");
-    let dots = document.getElementsByClassName("dot");
+    let slides = document.getElementsByClassName('favourite__slides');
+    let dots = document.getElementsByClassName('dot');
 
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "flex";
-  dots[slideIndex-1].className += " active";
+        slides[i].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+    slides[slideIndex - 1].style.display = 'flex';
+    dots[slideIndex - 1].className += ' active';
 }
 
-let timer = setInterval(function(){
+let timer = setInterval(function () {
     slideIndex++;
     showSlides(slideIndex);
-  }, 6000);
-
-
-
-   
-
-
-
+}, 6000);
