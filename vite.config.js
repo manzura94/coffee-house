021f21e737c3eb59125a129b1091f2c307fd6fdc
manzura94 <manzura_jabborova@mail.ru>
2025-10-27 13:11:57 +0500

    feat: change link paths

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ export default defineConfig({
   root: ".",
   build: {
     outDir: "dist",
+
+    rollupOptions:{
+     input:{
+      main: "./index.html",
+      menu: "./Menu/index.html",
+      register: "./register.html",
+      cart: "./cart.html",
+      signin: "./signin.html",
+
+
+    }
+  }
   },
   server: {
     open: true,
