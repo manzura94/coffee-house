07107193c4979653c1fa5697e27a0630b45f2372
manzura94 <manzura_jabborova@mail.ru>
2025-10-28 13:16:53 +0500

    fix: fix error

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: "./index.html",
-        menu: "./Menu/index.html",
+        menu: "./menu.html",
         register: "./register.html",
         cart: "./cart.html",
         signin: "./signin.html",
